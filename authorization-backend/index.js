07d970362f8c04467e51a65ca0888c560715d2b9
BoschPeter <bosch.peter@icloud.com>
2020-01-06 00:01:28 +0100

    tt

--- authorization-backend/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('discipl-core')
-
-ssidWebsiteClientAuth = core.newSsid('node')
-
-const onAuthRequest = (req, res) => {
-  let bsnlink = req.parameters['bsnlink']
-  if(session[‘bsn’] == core.get(bsnlink, ssidWebsiteClientAuth.did).data.BSN)
-    core.attest(ssidWebsiteClientAuth, ‘allowedBSN’, bsnlink)
-}
--- waardepapieren-service/README.md
+++
@@ -1,7 +0,0 @@
-# Waardepapieren service
-
-This service fulfills needs for waardepapieren
-
-# Build & Test
-
-Use `npm install` to build, `npm test` to test. `npm run system-test` would run a system test, but this will not function due to the NLX service being down.
